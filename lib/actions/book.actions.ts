5e2363c55d05eb3e40c60872006805cb6939d730
Phyo Htet Paing <phyohtetpaing200303@gmail.com>
2026-03-03 16:05:17 +0700

    feat: implement book upload functionality with form, server actions, and file upload API.

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -6,6 +6,25 @@ import { generateSlug, serializeData } from "../utils";
 import Book from "@/database/models/book.model";
 import BookSegment from "@/database/models/book-segment.model";
 
+export const getAllBooks = async () => {
+  try {
+    await connectDB();
+
+    const books = await Book.find().sort({ createdAt: -1 }).lean();
+
+    return {
+      success: true,
+      data: serializeData(books),
+    };
+  } catch (error) {
+    console.error("Error getting all books", error);
+    return {
+      success: false,
+      error: "Failed to get all books",
+    };
+  }
+};
+
 export const checkBookExists = async (title: string) => {
   try {
     await connectDB();
--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "covers.openlibrary.org",
       },
+      {
+        protocol: "https",
+        hostname: "raoih1nqnrzmbav3.public.blob.vercel-storage.com",
+      },
     ],
   },
   experimental: {
